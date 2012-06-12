d095766e676edfbcbbfbcd8217a366789cf1581b
splinterbrain <splinterbrain@gmail.com>
2012-06-12 00:53:06 -0700

    Static file server and profiling

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-// requires node's http module
-var http = require('http');
+require('nodetime').profile({stdout:false});
+var skeleton = require('skeleton');
+
 
 // creates a new httpServer instance
-var server = http.createServer(function(req, res) {
-	// this is the callback, or request handler for the httpServer
-
-	// respond to the browser, write some headers so the
-	// browser knows what type of content we are sending
-	res.writeHead(200, {
-		'Content-Type' : 'text/html'
-	});
-
-	// write some content to the browser that your user will see
-	res.write('<h1>hello, i know nodejitsu.</h1>');
-
-	// close the response
-	res.end();
-});
+var server = skeleton.createServer();
 
 //Check for production (nodejitsu) or development (localhost) and deploy on appropriate port
 if(process.env.NODEJS_ENV == "production"){
