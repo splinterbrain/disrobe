e501b5c776ab65fce21b4ba7eb2fa5abee66c07f
splinterbrain <splinterbrain@gmail.com>
2012-06-23 15:39:26 -0700

    Connecting to mongodb local without error

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 require('nodetime').profile({
     stdout : false
 });
+
+//For reasons passing understanding, the formatter toggle isn't working, thus the crazy long line of requires
 //@formatter:off
-var flatiron = require('flatiron'), app = flatiron.app, path = require('path'), ecstatic = require('ecstatic'), json = require('JSON');
+var flatiron = require('flatiron'), app = flatiron.app, path = require('path'), ecstatic = require('ecstatic'), json = require('JSON'), mongodb = require('mongodb');
 //@formatter:on
 
 var webroot = path.join(__dirname, 'public');
+var HTTP_PORT, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME;
+
+app.use(flatiron.plugins.log, {});
+
+//Set connection variables for production or development environments
+//Probably want to move these to nsconf or such eventually
+if(process.env.NODEJS_ENV == "production") {
+    HTTP_PORT = 80;
+    var njMongo = "mongodb://nodejitsu:b291c5811deafe3ed85f6b32c140a2df@staff.mongohq.com:10039/nodejitsudb951252111336"; 
+    //Thanks to http://joesul.li/van/blog/nodejitsu-node-mongo-native.html for parsing regex
+    var arr = /.*:\/\/(.*):(.*)@(.*):(.*)\/(.*)/.exec(njMongo);
+    DB_USER = arr[0];
+    DB_PASS = arr[1];
+    DB_HOST = arr[2];
+    DB_PORT = arr[3];
+    DB_NAME = arr[4];
+}else{
+    HTTP_PORT = 8080;
+    DB_HOST = "localhost";
+    DB_PORT = 27017; //Default mongod port
+    DB_NAME = "disrobe";
+}
+
+//Connect to mongod
+//Thanks again to ibid for help connecting when using native mongod
+app.log.info("Connecting to mongodb", [DB_HOST, DB_PORT]);
+var db = new mongodb.Db(DB_NAME, new mongodb.Server(DB_HOST, DB_PORT, {auto_reconnect: true}, {}));
+db.open(function(openError, openData){
+    if(openData){
+        if(DB_USER && DB_PASS){
+            openData.authenticate(DB_USER, DB_PASS, function(authError, authData){
+               if(authError) app.log.error(authError); 
+            });    
+        }else{
+            app.log.info
+        }        
+    }else{
+        app.log.error(openError);
+    }
+});
 
 //Evetually we'll want to use templates to generate static versions of html files
 //For now we just serve the mockups
@@ -41,12 +83,6 @@ app.router.configure({
     }
 });
 
-// Check for production (nodejitsu) or development (localhost) and deploy on
-// appropriate port
-if(process.env.NODEJS_ENV == "production") {
-    app.start(8080);
-    // the server will listen on port 80
-} else {
-    app.start(8080);
-    // the server will listen on port 8080
-}
+
+
+app.start(HTTP_PORT);
